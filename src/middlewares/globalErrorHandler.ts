d1c3bd2a91b4612ade3bc80a308e43de82b42a59
tanvirsp <tanvirgraphic@gmail.com>
2026-09-23 02:36:02 +0600

    Global Error handler code modify

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -56,13 +56,9 @@ export const globalErrorHandler = async (
   res.status(statusCode).json({
     success: false,
     statusCode: statusCode || httpStatus.INTERNAL_SERVER_ERROR,
-    name:
-      config.node_env === "development" ? errorName : "Internal Server Error",
-    message:
-      config.node_env === "development"
-        ? errorMessage
-        : "Internal Server Error",
-    error: config.node_env === "development" ? err : undefined,
-    stack: config.node_env === "development" ? err.stack : undefined,
+    name: errorName,
+    message: errorMessage,
+    error: err,
+    stack: err.stack,
   });
 };
